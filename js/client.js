9e452126952c7d90c3b3e6eeee20a7dd8c154be6
valideo <valentin.bredy@hotmail.fr>
2017-01-19 02:11:24 +0100

    tuto 35 min

--- js/client.js
+++ js/client.js
@@ -1,25 +1,65 @@
 (function($){
 
-	var socket = io.connect('http://localhost:8080');
-
+	var socket = io.connect('http://localhost:1010');
+	$('#username').focus();
+	//Recupération Login
 	$('#loginform').submit(function(event){
 		event.preventDefault();
-
 		socket.emit('login',{
-			pseudo : $('#username').val()
+			username : $('#username').val()
 		})
-	})
+	});
 
-	socket.on('newusr', function(user){
-		alert('Nouvel utilisateur' + user);
-	})
 
+	//Affichage interface et cacher page login
 	socket.on('logged', function(){
-		$('#login').fadeOut();s
+		$('#login').fadeOut();
+		$('.interface').fadeIn();
+		$('#message').focus();
+	});
+
+
+//Création des évènements de connexion
+
+	//Affichage de connexion sur chat
+	socket.on('newusr', function(user){
+		$('#zone_chat').append('<p class="messages-chat" style="color : #754220;"><em>' + user.pseudo + ' vient de se connecter</em></p>');
+		document.title = user.pseudo + ' | ' + document.title;
+	});
+
+	//Ajout de l'avatar sur la map
+	socket.on('newavatar', function(user){
+		$('#zone-animation').append('<img src="'+user.avatar+'" id="'+user.pseudo+'" alt="avatar" width="70px">');
+	});
+
+
+//Gestion de la déconnexion
+
+	//Suppression de l'avatar sur la map et affichage déconnexion chat
+	socket.on('disusr', function(user){
+		$('#' + user.pseudo).remove();
+		$('#zone_chat').append('<p class="messages-chat" style="color : #754220;"><em>' + user.pseudo + ' vient de se déconnecter</em></p>');
+	})
+
+
+//Gestion des messages
+	$('#form').submit(function(event){
+		event.preventDefault();
+		socket.emit('newmsg', {message : $('#message').val() });
+		$('#message').val('');
+		$('#message').focus();
+	});
+
+	socket.on('newmsg', function(message){
+		
 	})
 
 })(jQuery);
 
+
+
+/* a faire à la fin conditions pseudo
 while('#username'.length < 2 || '#username'.length > 15){
 	alert('Votre pseudo doit contenir entre 3 et 5 caractères !');
 }
+*/
--- server.js
+++ server.js
@@ -4,19 +4,45 @@ httpServer = http.createServer(function(req,res){
 	console.log('qqun est sur la page');
 });
 
-httpServer.listen(8080);
+httpServer.listen(1010);
 
 var io = require('socket.io').listen(httpServer);
+var users = {};
 
 io.sockets.on('connection', function(socket){
 
-	var me;
+	var me = false;
+	for(var k in users){
+		socket.emit('newavatar',users[k]);
+	}
 
+	//Connexion au chat
 	socket.on('login', function(user){
 		me = user;
-		me.id = user.username;
-		socket.broadcast.emit('newusr', me);
+		me.pseudo = user.username;
+		me.avatar = 'src/static-face.png';
 		socket.emit('logged');
-	})
+		socket.broadcast.emit('newusr', me);
+		io.sockets.emit('newavatar', me);
+		users[me.pseudo] = me;
+
+		//Deconnexion au chat
+		socket.on('disconnect', function(){
+			if(!me){
+				return false;
+			}
+			delete users[me.pseudo];
+			io.sockets.emit('disusr', me);
+		});
+
+	});
+
+	//Gestion des messages
+	socket.on('newmsg',function(message){
+		message.user = me;
+		date = new Date();
+		message.h = date.getHours();
+		message.m = date.getMinutes();
+		io.sockets.emit('newmsg', message);
 
 });
